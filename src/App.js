10e0269c80496ec2d28d1784069cbdfdf08b03f5
Douglas Dantas <douglasdantas@MacBook-Pro-de-Douglas.local>
2023-07-01 19:04:32 -0300

    add content for page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import HeaderComponent from './components/HeaderComponent/HeaderComponent';
-
+import ContentComponent from './components/ContentComponent/ContentComponent'
 import FooterComponent from './components/FooterComponent/FooterComponent';
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent/>
+      <ContentComponent/>
       <FooterComponent/>
     </div>
   );
--- src/components/ContentComponent/ContentComponent.js
+++ src/components/ContentComponent/ContentComponent.js
@@ -1,8 +1,42 @@
 import styles from "./ContentComponent.module.css"
+import Button from "../Button/Button"
+import privacityLogo from "../../assets/img/privacitySecurityIcon.svg"
+import googleLogo from "../../assets/img/logoGoogle.svg"
+import searchIcon from "../../assets/img/searchIcon.svg"
+import keyboardIcon from "../../assets/img/keyboardicon.svg"
+import microphoneIcon from "../../assets/img/microphoneIcon.svg"
 
 function ContentComponent(){
     return (
-        <h1>HelloWorld</h1>       
+        <>
+        <section className={styles.googleLogo}>
+            <img src={googleLogo} alt=""></img>
+        </section>
+
+        <section className={styles.searchBar}>
+            <div className={styles.leftSearchBar}>
+                <img src={searchIcon} alt=""/>
+            </div>
+            <div className={styles.rightSearchBar}>
+                <img src={keyboardIcon} alt=""/>
+                <img src={microphoneIcon} alt=""/>
+            </div>
+        </section>
+
+        <section className={styles.buttons}>
+            <Button text="Pesquisa Google"/>
+            <Button text="Estou com sorte"/>
+        </section>
+
+        <section className={styles.privacityArea}>
+            <div>
+                <img src={privacityLogo} alt=""/>
+            </div>
+            <div>
+                <span>Faça o Check-up de privacidade</span>
+            </div>
+        </section>
+        </>
     )
 }
 
